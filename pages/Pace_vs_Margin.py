22039b92886ce7c44eb1fc15c3f52febb4f0d279
Michael Torres <michael.torres@lennar.com>
2025-07-30 12:14:45 -0500

    Auto-push update

--- pages/Pace_vs_Margin.py
+++ pages/Pace_vs_Margin.py
@@ -36,7 +36,7 @@ with st.sidebar:
     target_date = target_date_input
 
     # COE Date Range filter
-    coe_range_input = st.date_input("COE Date Range", value=(datetime.date(2025, 6, 1), datetime.date(2025, 8, 31)))
+    coe_range_input = st.date_input("COE Date Range", value=(datetime.date(2025, 7, 1), datetime.date(2025, 9, 15)))
     st.session_state["pace_margin_est_coe_range"] = coe_range_input
     if isinstance(coe_range_input, tuple) and len(coe_range_input) == 2:
         est_coe_start, est_coe_end = pd.to_datetime(coe_range_input[0]), pd.to_datetime(coe_range_input[1])
